18d3ac4ef987bd2f2cf1568110148ea7acb01464
Kwinsi Thakkar <kwinsi.thakkar@simformsolutions.com>
2026-05-20 10:42:22 +0530

    redeploy static web app -2

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: "https://wa-crud-learn-03-fngfgpbbb8apfugp.eastasia-01.azurewebsites.net"
 });
 export default api;
